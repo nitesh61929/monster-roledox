163c49ce66d80e33995c454d9257cf14c1acc487
nitesh <nitesh61929@gmail.com>
2020-10-08 16:55:46 +0545

    Updates

---
+++ precache-manifest.c7b4196813028a32a564dd7ba1d409c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3101b3f29bb7c6876929bb578a50562",
+    "url": "/monsters-roledox/index.html"
+  },
+  {
+    "revision": "61daedccd2453028a9f1",
+    "url": "/monsters-roledox/static/css/main.455b1fac.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-roledox/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-roledox/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61daedccd2453028a9f1",
+    "url": "/monsters-roledox/static/js/main.786e3cb4.chunk.js"
+  },
+  {
+    "revision": "7a2bb6dde0b29f300683",
+    "url": "/monsters-roledox/static/js/runtime-main.95b82ba4.js"
+  }
+]);
